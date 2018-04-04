a968c0655e901cf906abb9f08668ebe8d742d7e2
Mukhamad Syafii <fiinter302@gmail.com>
2018-04-05 01:05:40 +0900

    Membuat Atribut dan behavior

--- src/praktek1/Data_Sepeda_Motor.java
+++ src/praktek1/Data_Sepeda_Motor.java
@@ -10,5 +10,18 @@ package praktek1;
  * @author Macan Sakti
  */
 public class Data_Sepeda_Motor {
+    String Produk;
+    String Merek;
+    String Warna;
+    String Kapasitas;
+    String Type;
+    
+    void cetakInfo(){
+        System.out.println("Produk:"+ Produk);
+        System.out.println("Merek:"+ Merek);
+        System.out.println("Warna:"+ Warna);
+        System.out.println("Kapasitas:"+ Kapasitas);
+        System.out.println("Type:"+ Type);
+    }
     
 }
