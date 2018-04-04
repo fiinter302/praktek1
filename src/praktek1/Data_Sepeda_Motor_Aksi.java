8b9062f812a99fd4af275bc8341f760cd0305314
Mukhamad Syafii <fiinter302@gmail.com>
2018-04-05 01:24:43 +0900

    instance Object

--- src/praktek1/Data_Sepeda_Motor_Aksi.java
+++ src/praktek1/Data_Sepeda_Motor_Aksi.java
@@ -11,7 +11,12 @@ package praktek1;
  */
 public class Data_Sepeda_Motor_Aksi {
     public static void main(String[] args) {
+        Data_Sepeda_Motor Yamaha = new Data_Sepeda_Motor();
+        Data_Sepeda_Motor Honda = new Data_Sepeda_Motor();
+        Data_Sepeda_Motor Kawasaki = new Data_Sepeda_Motor();
         
+     
+    
     }
     
 }
