a0dfb15bf8d179dce2815f62a27dbde8eedccd16
Mukhamad Syafii <fiinter302@gmail.com>
2018-04-05 01:28:58 +0900

    Mengisi Data Atribut

--- src/praktek1/Data_Sepeda_Motor_Aksi.java
+++ src/praktek1/Data_Sepeda_Motor_Aksi.java
@@ -15,6 +15,24 @@ public class Data_Sepeda_Motor_Aksi {
         Data_Sepeda_Motor Honda = new Data_Sepeda_Motor();
         Data_Sepeda_Motor Kawasaki = new Data_Sepeda_Motor();
         
+        Yamaha.Produk="Yamaha";
+        Yamaha.Merek="Vega";
+        Yamaha.Kapasitas="113cc";
+        Yamaha.Warna="Hitam";
+        Yamaha.Type="Bebek";
+        
+        Honda.Produk="Honda";
+        Honda.Merek="CB";
+        Honda.Kapasitas="150cc";
+        Honda.Warna="Putih";
+        Honda.Type="Naket";
+        
+        Kawasaki.Produk="Kawasaki";
+        Kawasaki.Merek="Ninja";
+        Kawasaki.Kapasitas="250cc";
+        Kawasaki.Warna="Hijau";
+        Kawasaki.Type="Sport";
+        
      
     
     }
